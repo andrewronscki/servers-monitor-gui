128dbaf55e8ce66d3be1b66c5dac979c82516a1e
eduwr <eduwric@gmail.com>
2019-11-20 23:27:20 -0300

    Criacao da página de login

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Box = styled.div`
+  margin: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  /* background-color: #fff; */
+`;
+
+export const Title = styled.h1`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  color: #0a6ecf;
+  margin-bottom: 20px;
+`;
+
+export const LogoImg = styled.img`
+  height: 80%;
+`;
+
+export const UsrImg = styled.img`
+  height: 120px;
+`;
+
+export const LoginForm = styled.form`
+  padding: 20px;
+  display: flex;
+  height: auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #053e75;
+  box-shadow: 10px 10px 50px #000000;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #000307;
+  background-color: #c1c1c1;
+  border-radius: 4px;
+  color: #666;
+  width: 100%;
+  margin: 8px 0px 0px 0px;
+  padding: 5px;
+`;
+
+export const Btn = styled.button`
+  background: #0a6ecf;
+  border: 1px solid #000307;
+  margin: 8px 0px 0px 0px;
+  width: 100%;
+  padding: 5px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  color: #ffffff;
+`;
+
+export const Link = styled.a`
+  margin-top: 3px;
+  font-size: 10px;
+  font-weight: normal;
+  padding: 3px;
+  text-decoration: none;
+  color: #fff;
+`;
